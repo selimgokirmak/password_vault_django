beea12be5aace76184b78a068f29c0e97f91cef8
selimgokirmak <86617834+selimgokirmak@users.noreply.github.com>
2022-06-27 13:37:21 +0300

    index view eklendi

--- passwords/views.py
+++ passwords/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Email, Password
+
+def index(request):
+    emails = Email.objects.all()
+
+    context = {
+        "emails": emails,
+    }
+
+    return render(request, "index.html", context)
